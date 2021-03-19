a18d2ad715cdf90b881fd409ab1d52c35f4bc28b
Hussein <Husseinhewehii@gmail.com>
2021-03-19 06:47:24 +0200

    m

--- app/Listeners/UserListener.php
+++ app/Listeners/UserListener.php
@@ -3,25 +3,15 @@
 namespace App\Listeners;
 
 use App\Events\UserCreatedEvent;
+use App\Jobs\SendEmailJob;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\SendEmail;
 
 class UserListener
 {
     public function handleCreatedUser(UserCreatedEvent $event)
     {
         $user = $event->user;
-        // $to_name = $user->name;
-        // $to_email = $user->email;
-        // $data = array('name'=>'Laravel Admin', 'body' => 'Your Account Has Been Registered Successfully');
-
-        // Mail::send('emails.mail', $data, function($message) use ($to_name, $to_email) {
-        // $message->to($to_email, $to_name)
-        // ->subject('Welcome To SeoEra');
-        // $message->from(env('MAIL_USERNAME'),'Congratulations');
-        // });
 
         $data = array(
             'name' => $user->name,
@@ -29,7 +19,8 @@ class UserListener
             'subject' => 'Welcome to Seoera',
             'body' => 'Your Account Has Been Registered Successfully',
           );
-          Mail::to($data['email'])->send(new SendEmail($data));
+        dispatch(new SendEmailJob($data));
+        
 
     }
 }
